9ab0f5e68d47d68e0bdfedc4d20bc5520b4310eb
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2017-05-04 13:41:45 +0430

    rename mocule

--- config/config.php
+++ config/config.php
@@ -15,14 +15,14 @@ array_insert($GLOBALS['BE_MOD']['content'], 1, array
 (
 	'airquality' => array
 	(
-		'tables' => array('tl_airquality_city','tl_airquality_station','tl_airquality_data'),
-		'icon'   => 'system/modules/airquality/assets/icon.png',
-		'stylesheet'   => 'system/modules/airquality/assets/styles/be.css',
+		'tables'     => array('tl_airquality_city','tl_airquality_station','tl_airquality_data'),
+		'icon'       => 'system/modules/airquality/assets/icon.png',
+		'stylesheet' => 'system/modules/airquality/assets/styles/airquality_be.css',
 	)
 ));
 
 /**
  * Front end modules
  */
-$GLOBALS['FE_MOD']['airquality']['mod_airquality_widget'] = 'ModuleAirQualityWidget';
-$GLOBALS['FE_MOD']['airquality']['mod_airquality_full']   = 'ModuleAirQualityFull';
+$GLOBALS['FE_MOD']['airquality']['mod_airquality_widget'] = 'Respinar\AirQuality\ModuleAirQualityWidget';
+$GLOBALS['FE_MOD']['airquality']['mod_airquality_chart']  = 'Respinar\AirQuality\ModuleAirQualityChart';
